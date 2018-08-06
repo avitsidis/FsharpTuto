f6a965ee67308b407210d0dc131d4bbde015b96a
Avit Sidis <avit.sidis@infrabel.be>
2018-08-06 09:56:49 +0200

    add hello world and setup project

---
+++ FSharpTuto.Console/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    //Print text to console
+    let name = Environment.UserName
+    printfn "Hello %s!" name
+
+
+    0 // return an integer exit code
